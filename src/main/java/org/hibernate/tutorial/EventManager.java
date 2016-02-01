90f89589688fcb7d3278deab2b25ab3a062b85eb
javanewcomer <tomsubscriber@gmail.com>
2016-02-01 22:38:04 +0100

    getCrrentSession and save assiocated relation

--- src/main/java/org/hibernate/tutorial/EventManager.java
+++ src/main/java/org/hibernate/tutorial/EventManager.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.hibernate.Session;
 import org.hibernate.tutorial.hbm.Event;
+import org.hibernate.tutorial.hbm.Person;
 import org.hibernate.tutorial.util.HibernateUtil;
 
 public class EventManager {
@@ -11,14 +12,22 @@ public class EventManager {
   public static void main (String[] args) {
     EventManager mgr = new EventManager ();
 
-    mgr.createAndStoreEvent ("My Event", new Date ());
+    long eventID = mgr.createAndStoreEvent ("My Event", new Date ());
+    long personId = mgr.createAndStorePerson (18, "mueller", "perter");
+
+    mgr.addPersonToEvent (personId, eventID);
+
+    long eventID2 = mgr.createAndStoreEvent ("My Event2", new Date ());
+    long personId2 = mgr.createAndStorePerson (18, "mueller2", "perter2");
+
+    mgr.addPersonToEvent2 (personId2, eventID2);
 
     HibernateUtil.getSessionFactory ().close ();
   }
 
-  private void createAndStoreEvent (String title,
+  private long createAndStoreEvent (String title,
                                     Date theDate) {
-    Session session = HibernateUtil.getSessionFactory ().openSession ();
+    Session session = HibernateUtil.getSessionFactory ().getCurrentSession ();
     session.beginTransaction ();
 
     Event theEvent = new Event ();
@@ -27,6 +36,61 @@ public class EventManager {
     session.save (theEvent);
 
     session.getTransaction ().commit ();
+    return theEvent.getId ();
+  }
+
+  private long createAndStorePerson (int age,
+                                     String firstname,
+                                     String lastname) {
+    Session session = HibernateUtil.getSessionFactory ().getCurrentSession ();
+    session.beginTransaction ();
+
+    Person person = new Person ();
+    person.setAge (age);
+    person.setFirstname (firstname);
+    person.setLastname (lastname);
+
+    session.save (person);
+
+    session.getTransaction ().commit ();
+    return person.getId ();
+  }
+
+  private void addPersonToEvent (Long personId,
+                                 Long eventId) {
+    Session session = HibernateUtil.getSessionFactory ().getCurrentSession ();
+    session.beginTransaction ();
+
+    Person aPerson = (Person) session.load (Person.class, personId);
+    Event anEvent = (Event) session.load (Event.class, eventId);
+    aPerson.getEvents ().add (anEvent);
+
+    session.getTransaction ().commit ();
+  }
+
+  private void addPersonToEvent2 (Long personId,
+                                  Long eventId) {
+    Session session = HibernateUtil.getSessionFactory ().getCurrentSession ();
+    session.beginTransaction ();
+
+    Person aPerson = (Person) session.createQuery ("select p from Person p left join fetch p.events where p.id = :pid")
+                                     .setParameter ("pid", personId)
+                                     .uniqueResult (); // Eager fetch the collection so we can use it detached
+    Event anEvent = (Event) session.load (Event.class, eventId);
+
+    session.getTransaction ().commit ();
+
+    // End of first unit of work
+
+    aPerson.getEvents ().add (anEvent); // aPerson (and its collection) is detached
+
+    // Begin second unit of work
+
+    Session session2 = HibernateUtil.getSessionFactory ().getCurrentSession ();
+    session2.beginTransaction ();
+    session2.update (aPerson); // Reattachment of aPerson
+
+    session2.getTransaction ().commit ();
   }
 
 }
